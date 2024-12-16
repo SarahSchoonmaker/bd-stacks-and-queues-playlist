5641a7b58dded24cdf44b328c0448290b2f2afb6
sarahschoonmaker <srschoonmaker@gmail.com>
2024-12-15 22:11:51 -0500

    edit readme

--- src/SentenceBuilder.java
+++ src/SentenceBuilder.java
@@ -1,10 +1,14 @@
+import java.util.Stack;
+
 public class SentenceBuilder {
+    private Stack<String> sentenceStack;
 
     /**
      * Constructor.
      */
     public SentenceBuilder() {
-
+        // Initialize the stack to store the words of the sentence
+        sentenceStack = new Stack<>();
     }
 
     /**
@@ -12,15 +16,17 @@ public class SentenceBuilder {
      * @param word to be added to the sentence.
      */
     public void addWord(String word) {
-
+        // Push the word onto the stack
+        sentenceStack.push(word);
     }
 
     /**
      * Undoes the last word added, and returns it.
-     * @return The word most recently added to the sentence, if any; null, otherwise.
+     * @return The word most recently added to the sentence, if any; null otherwise.
      */
     public String undo() {
-        return null;
+        // Return the top word if the stack is not empty, otherwise return null
+        return sentenceStack.isEmpty() ? null : sentenceStack.pop();
     }
 
     /**
@@ -28,6 +34,7 @@ public class SentenceBuilder {
      * @return string representation of the words currently in the sentence.
      */
     public String getSentenceWords() {
-        return "[]";
+        // Return the stack as a formatted string (e.g., [word1, word2, ...])
+        return sentenceStack.toString();
     }
 }
